5f27fe235422eda607dbebf00bee2cfdea4299a5
karthik R <karthikioi98@gmail.com>
2021-06-11 18:17:33 +0530

    Added API to fetch Weather Data

--- DjangoWeatherApplication/core/views.py
+++ DjangoWeatherApplication/core/views.py
@@ -2,11 +2,29 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
 from rest_framework import status
 from django.shortcuts import render
+
+API_KEY = 'a948ab4862c0d6055597ff96bec0f06d'
+cities = ['London', 'Paris', 'Mumbai', 'Delhi', 'Kolkata', 'France', 'Tokyo', 'Berlin']
 
 
 @api_view(['GET'])
 def my_view(request):
     return Response(status=status.HTTP_200_OK, data="Hello")
+
+
+@login_required
+@api_view(['GET'])
+def get_weather_data(request):
+    try:
+        data = {}
+        for city in sorted(cities):
+            response = requests.post('http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city, API_KEY))
+            response = response.json()
+            data[city] = response
+        return render(request, 'weather.html', context={'Data': data})
+    except Exception as e:
+        return Response(status=status.HTTP_400_BAD_REQUEST, data=e)
 
--- DjangoWeatherApplication/urls.py
+++ DjangoWeatherApplication/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
+from DjangoWeatherApplication.core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('hello/', login_required(TemplateView.as_view(template_name='hello.html')), name='hello'),
+    path('get_weather_data/', login_required(views.get_weather_data), name='weather')
 ]
