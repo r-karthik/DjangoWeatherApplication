4430f9132eaacc8631ca928f6c35bf6e162a38be
karthik R <karthikioi98@gmail.com>
2021-06-11 16:15:55 +0530

    Added User Authentication Login/Logout

--- DjangoWeatherApplication/core/views.py
+++ DjangoWeatherApplication/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import render
 
-# Create your views here.
+
+@api_view(['GET'])
+def my_view(request):
+    return Response(status=status.HTTP_200_OK, data="Hello")
+
