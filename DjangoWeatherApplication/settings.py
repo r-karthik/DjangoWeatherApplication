ddcb314f45d308145efeecc2f7a88badd7342df1
r-karthik <30434242+r-karthik@users.noreply.github.com>
2021-07-24 23:05:48 +0530

    Update settings.py

--- DjangoWeatherApplication/settings.py
+++ DjangoWeatherApplication/settings.py
@@ -154,7 +154,7 @@ CELERY_TASK_SERIALIZER = "json"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465
-EMAIL_HOST_USER = "karthikioi98@gmail.com"
+EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = False
 EMAIL_USE_TLS = False
